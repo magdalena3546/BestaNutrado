bd67c140d8fc3a0f6ff0ae4f4a08843ff3fd1723
Magdalena <magdalenamalek52@gmail.comgit statusgit status git config --global user.email magdalenamalek52@gmail.com>
2023-03-02 15:34:07 +0100

    add SectionComposition styles and structure

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,12 @@
 import BannerBottom from "../../views/BannerBottom/BannerBottom";
 import Header from "../../views/Header/Header";
+import SectionComposition from "../../views/SectionComposition/SectionComposition";
 
 const Home = () => {
     return(
     <>
         <Header />
+        <SectionComposition />
         <BannerBottom />
     </>
     )

ae654b2bf6d9ad8460ab412d884cada42c37c31c
Magdalena <magdalenamalek52@gmail.comgit statusgit status git config --global user.email magdalenamalek52@gmail.com>
2023-03-09 17:47:22 +0100

    add Products styles and structure

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,6 +2,7 @@ import BannerBottom from "../../views/BannerBottom/BannerBottom";
 import Header from "../../views/Header/Header";
 import SectionComposition from "../../views/SectionComposition/SectionComposition";
 import Blog from "../../views/Blog/Blog";
+import Products from "../../views/Products/Products";
 
 const Home = () => {
     return(
@@ -9,6 +10,7 @@ const Home = () => {
         <Header />
         <SectionComposition />
         <BannerBottom />
+        <Products />
         <Blog />
     </>
     )
---
+++ src/views/Products/Products.js
@@ -0,0 +1,21 @@
+import Button from '../../common/Button/Button';
+import SectionTitle from '../../common/SectionTitle/SectionTitle';
+import styles from './Products.module.scss';
+
+const Products = () => {
+    return (
+        <div className={styles.container}>
+            <SectionTitle name="Nasze produkty"/>
+            <div className={styles.content}>
+                <div><img src='./images/dogProduct.png' alt="biały pies"></img></div>
+                <div>
+                    <h4>Poznaj nasze produkty!</h4>
+                    <p>Zapoznaj się z gamą naszych produktów. Wybierz idealną karmę dla twojego pupila!</p>
+                    <Button name="Więcej" />
+                </div>
+                <div className={styles.photo}><img src='./images/besta1.png' alt="opakowanie karmy Besta Nutrado"></img></div>
+            </div>
+        </div>
+    );
+};
+ export default Products;

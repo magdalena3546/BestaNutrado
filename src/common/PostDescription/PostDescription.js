6059abf3bb37bfea87072af8c6d8e3812d0ffa15
Magdalena <magdalenamalek52@gmail.comgit statusgit status git config --global user.email magdalenamalek52@gmail.com>
2023-06-27 17:14:21 +0200

    change size in postDescription, change logo, build project

--- src/common/PostDescription/PostDescription.js
+++ src/common/PostDescription/PostDescription.js
@@ -1,16 +1,18 @@
-import styles from './PostDescription.module.scss';
-import Button from '../Button/Button';
+import styles from "./PostDescription.module.scss";
+import Button from "../Button/Button";
 
-const PostDescription = props => {
-  return(
+const PostDescription = (props) => {
+  return (
     <div className={styles.box}>
-      <div className={styles.image}><img src={"./images/" + props.image} alt={props.alt}/></div>
+      <div className={styles.image}>
+        <img src={"./images/" + props.image} alt={props.alt} />
+      </div>
       <p className={styles.date}>{props.publishedDate}</p>
       <h6>{props.title}</h6>
-      <p>{props.shortDescription}</p>
+      <p className={styles.text}>{props.shortDescription}</p>
       <Button link={`/post/${props.id}`} name="więcej"></Button>
     </div>
-  )
+  );
 };
 
 export default PostDescription;
